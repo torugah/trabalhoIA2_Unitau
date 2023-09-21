e875ae4f7b2dd8b12e640b412ed4f72758a139a6
torugah <fillipibarros@gmail.com>
2023-09-21 20:15:45 -0300

    Adição de dois novos arquivos de texto/modelo para grafo.

--- src/main/java/com/example/demo/controller/CaminhoController.java
+++ src/main/java/com/example/demo/controller/CaminhoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/caminho")
+@RequestMapping(value = "/caminho") 
 public class CaminhoController {
     
     @GetMapping(value = "/{origem}/{destino}/{metodo}")
